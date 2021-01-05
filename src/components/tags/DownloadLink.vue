2b17b2b8792bfb8301d1fc3a296a5aea53fe75f9
semenInRussia <semenprofis999@gmail.com>
2021-01-06 02:01:29 +0300

    add DownloadLink

---
+++ src/components/tags/DownloadLink.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="download-link">
+    <slot @contextmenu="openMenu"/>
+
+    <div class="buttons" v-if="show">
+      <v-btn
+          :href="object.link"
+          color="yellow">
+        Download
+      </v-btn>
+
+      <v-btn @click="redirect">
+        View
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DownloadLink",
+
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    openMenu(e) {
+      this.show = !(this.show)
+
+      e.preventDefault()
+    },
+
+    redirect() {
+      this.object.$entity.redirect(this.$router)
+    }
+  },
+
+  props: {
+    object: {}
+  }
+}
+</script>
+
+<style scoped></style>
