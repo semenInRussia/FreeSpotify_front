b492f7857117fb6358bae2073d3f7d327b4b82be
Semen <hrams205@gmail.com>
2021-05-29 15:44:31 +0300

    [ROUTING: feature] Added routing for AristDetail

--- src/appRoutings.js
+++ src/appRoutings.js
@@ -2,9 +2,24 @@ import React from "react";
 
 import Artists from "./pages/Artists";
 import Home from "./pages/Home";
+import ArtistDetail from "./pages/ArtistDetail";
 
 
 export const routings = [
-    {url: "/", name: "My Home", component: (<Home/>)},
-    {url: "/artists", name: "Artists", component: (<Artists/>)}
+    {
+        url: "/",
+        name: "My Home",
+        component: (<Home/>)
+    },
+    {
+        url: "/search/artists",
+        name: "Artists Search",
+        component: (<Artists/>)
+    },
+    {
+        url: "/artists/:name",
+        name: "Artist Detail",
+        component: (<ArtistDetail/>),
+        notViewInNavbar: true
+    }
 ]
